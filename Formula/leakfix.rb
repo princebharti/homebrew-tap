5f4962f9516dff97253b96011e0639c1ea2ff086
Prince Bharti <prince.bharti@telekom-digital.com>
2026-03-15 02:18:34 +0530

    chore: update sha256 for v1.1.0

--- Formula/leakfix.rb
+++ Formula/leakfix.rb
@@ -2,7 +2,7 @@ class Leakfix < Formula
   desc "One-stop CLI tool to detect, remove and prevent secrets in git repositories"
   homepage "https://github.com/princebharti/leakfix"
   url "https://github.com/princebharti/leakfix/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "1fd8cbd5fbd32ca905b83ca04e6393fb3da1fd1cace213ce50f6a2c0605f380c"
+  sha256 "e2b4b05339cac6459e22810a03214a59fe7f2e2b73aa024dc8f1f15e31ed5eee"
   license "MIT"
 
   depends_on "python@3.11"
